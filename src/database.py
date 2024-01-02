5f32d127615a8d0f8343aa60720c41774ed2438a
nikita-th <nikitath@cybage.com>
2024-01-02 23:07:55 +0530

    database file updated

--- src/database.py
+++ src/database.py
@@ -2,10 +2,10 @@ import sqlalchemy
 import os
 from flask_sqlalchemy import SQLAlchemy
 
-db_user = os.environ.get('DB_USER_NAME')
-db_password = os.environ.get('DB_PASSWORD')
-db_name = os.environ.get('DB_NAME')
-db_host = os.environ.get('DB_HOST')
+db_user = 'ctgdevops'
+db_password = 'devops123'
+db_name = 'ctg_devops_ktracker'
+db_host = '34.66.229.117
 
 db_url = f"postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}"
 db = SQLAlchemy()
